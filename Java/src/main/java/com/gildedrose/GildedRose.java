799c2cf4b985aa49c0c355cde9ab6d6fecb2ce66
Kyrylo Medar <146186708+KirkaaTyapkaa@users.noreply.github.com>
2024-11-25 22:39:00 +0300

    Update2 GildedRose.java
    Створив окремий метод для порівняння елементів зі стрінгами, які також виніс окремо, та повставляв це в код

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,24 +3,33 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    private static final String Sulf = "Sulfuras, Hand of Ragnaros";
+    private static final String Aged = "Aged Brie";
+    private static final String BackPass = "Backstage passes to a TAFKAL80ETC concert";
+
+    
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    private boolean checkEqual(Item item, String check) {
+        return item.name.equals(check);
+    }
+
+    
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!checkEqual(items[i], Aged) && !checkEqual(items[i], BackPass)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                   if (!checkEqual(items[i], Sulf)) {
                         --items[i].quality;
                     }
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    ++items[i].quality;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (checkEqual(items[i], BackPass)) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 ++items[i].quality;
@@ -36,15 +45,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!checkEqual(items[i], Sulf)) {
                 --items[i].sellIn;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!checkEqual(items[i], Aged)) {
+                    if (!(checkEqual(items[i], BackPass)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!checkEqual(items[i], Sulf)) {
                                 --items[i].quality;
                             }
                         }
