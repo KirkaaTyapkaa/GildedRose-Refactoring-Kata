fd7bca94254011fe2cc5dcd9fbff699844e400df
Kyrylo Medar <146186708+KirkaaTyapkaa@users.noreply.github.com>
2024-11-26 16:07:46 +0300

    Update6 GildedRose.java
    Привів головний цикл фор до одного рівня іфів

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,7 +7,6 @@ class GildedRose {
     private static final String Aged = "Aged Brie";
     private static final String BackPass = "Backstage passes to a TAFKAL80ETC concert";
 
-    
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -17,42 +16,41 @@ class GildedRose {
     }
 
     private boolean checkNoSpecial(Item item) {
-    return !checkEqual(item, Aged) 
-            && !checkEqual(item, BackPass) 
-            && !checkEqual(item, Sulf) 
-            && item.quality > 0;
+        return !checkEqual(item, Aged) 
+                && !checkEqual(item, BackPass) 
+                && !checkEqual(item, Sulf) 
+                && item.quality > 0;
     }
 
     private boolean qualityLowThen50(Item item) {
-    return item.quality < 50;
+        return item.quality < 50;
     }
 
     private void increaseIfSellin(Item item) {
-    if (item.sellIn < 11 && qualityLowThen50(item)) {
-        ++item.quality;
-    }
-    if (item.sellIn < 6 && qualityLowThen50(item)) {
-        ++item.quality;
-    }
+        if (item.sellIn < 11 && qualityLowThen50(item)) {
+            ++item.quality;
+        }
+        if (item.sellIn < 6 && qualityLowThen50(item)) {
+            ++item.quality;
+        }
     }
 
     private void processQualityIncrease(Item item) {
-    if (qualityLowThen50(item)) {
-        ++item.quality;
+        if (qualityLowThen50(item)) {
+            ++item.quality;
 
-        if (checkEqual(item, BackPass)) {
-            increaseIfSellin(item);
+            if (checkEqual(item, BackPass)) {
+                increaseIfSellin(item);
+            }
         }
     }
-    }
 
-     public void updateQuality() {
+    public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (checkNoSpecial(items[i])) {
-                    --items[i].quality;
-                } else {
-               processQualityIncrease(items[i]);
+                --items[i].quality;
             }
+            processQualityIncrease(items[i]);
 
             if (!checkEqual(items[i], Sulf)) {
                 --items[i].sellIn;
@@ -61,20 +59,20 @@ class GildedRose {
             if (items[i].sellIn >= 0) {
                 continue;
             }
-            if (checkEqual(items[i], Aged)) {
-                if (qualityLowThen50(item)) {
-                    ++items[i].quality;
-                    }
-                return;
+
+            if (checkEqual(items[i], Aged) && qualityLowThen50(items[i])) {
+                ++items[i].quality;
+                continue;
+            }
+            if (checkEqual(items[i], BackPass)) {
+                items[i].quality = 0;
+                continue;
             }
 
-                
-            if (!checkEqual(items[i], BackPass) && !checkEqual(items[i], Sulf) && items[i].quality > 0) {
+            if (items[i].quality > 0) {
                 --items[i].quality;
-                return;
-            } 
-            items[i].quality = 0;
-                    
+            }
         }
     }
 }
+
